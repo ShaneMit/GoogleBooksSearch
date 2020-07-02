2c2d312e42f48ef1987dd06b045c78b4443d93be
ShaneMit <shanemit@cox.net>
2020-07-02 00:21:20 -0700

    worked on home page functions

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import axios from 'axios'
 
 const Home = () => {
+  const [ bookState, setBookState ] = useState({
+    search: '',
+    books: []
+  })
+
+  bookState.handleInputChange = event => {
+    setBookState({ ...bookState, [event.target.name]: event.target.value })
+  }
+
+  bookState.handleSearchBook = event => {
+    event.preventDefault()
+    axios.get(`/api/googlebooks/${bookState.search}`)
+      .then(({ data }) => {
+        console.log(data)
+      })
+      .catch(err => console.error(err))
+  }
+
   return(
     <>
-      <h1>This is the Home page</h1>
+      <form onSubmit={bookState.handleSearchBook}>
+        <TextField 
+          label="Search Google" 
+          name="search"
+          value={bookState.search}
+          onChange={bookState.handleInputChange}/>
+        <Button 
+        variant="outlined" 
+        color="primary"
+        onClick={bookState.handleSearchBook}>
+          Search
+      </Button>
+      </form>
     </>
   )
 }
