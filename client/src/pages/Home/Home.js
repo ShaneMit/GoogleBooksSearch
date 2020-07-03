4e9fe4e73b38a405f77bcbe7306775ea74b22387
ShaneMit <shanemit@cox.net>
2020-07-02 17:24:56 -0700

    Added cards that render on search

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,26 @@
 import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import CardHeader from '@material-ui/core/CardHeader'
 import axios from 'axios'
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+})
+
 const Home = () => {
+  const classes = useStyles()
   const [ bookState, setBookState ] = useState({
     search: '',
     books: []
@@ -17,7 +34,7 @@ const Home = () => {
     event.preventDefault()
     axios.get(`/api/googlebooks/${bookState.search}`)
       .then(({ data }) => {
-        console.log(data)
+        setBookState({ ...bookState, books: data })
       })
       .catch(err => console.error(err))
   }
@@ -37,6 +54,28 @@ const Home = () => {
           Search
       </Button>
       </form>
+      <div>
+        {
+          bookState.books.map(book => (
+            <Card className={classes.root}>
+              <CardHeader
+                title={book.volumeInfo.title}
+              />
+                <CardMedia
+                  className={classes.media}
+                  image={book.volumeInfo.imageLinks.smallThumbnail}
+                  title={book.volumeInfo.title}
+                />
+
+              <CardActions>
+                <Button size="small" color="primary">
+                  Save
+                </Button>
+              </CardActions>
+            </Card>
+          ))
+        }
+      </div>
     </>
   )
 }
